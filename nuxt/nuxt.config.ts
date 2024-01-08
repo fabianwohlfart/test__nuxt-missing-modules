3b714487be744bafb8b43ef2f3489697461b2f41
Fabian Wohlfart-Blank <fabianwohlfartblank@Carminas-MBP.local>
2024-01-08 11:10:39 +0100

    update: path to layer

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,5 +1,5 @@
 import  { resolve } from 'path'
-const layerDir = resolve('../..', '_Another', 'another__foundation', 'test')
+const layerDir = resolve('..', 'layer')
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
